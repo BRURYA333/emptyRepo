f575e47c592f700784540273b1aa8c32881a1cfb
teacherGit <n@n.com>
2024-01-21 20:26:42 +0200

    to 3 /

--- src/components/AddService.jsx
+++ src/components/AddService.jsx
@@ -6,7 +6,7 @@ const AddService = () => {
     const [newService, setNewService] = useState({});
 
     function changeProp(e) {
-        //copy from state form example
+        ///copy from state form example
         setNewService({
             ...newService,
             [e.target.id]: e.target.value
